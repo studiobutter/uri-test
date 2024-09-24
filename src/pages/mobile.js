7edfc9f5e5b6051d543a03628b61b8d3e5ee700f
studiobuttermedia <bebosua05@yahoo.com.vn>
2024-09-24 10:27:48 +0700

    Added Vietnam Region (Economy is in shambles) and Cloud Version of Genshin and Star Rail

--- src/pages/mobile.js
+++ src/pages/mobile.js
@@ -11,20 +11,48 @@ function Mobile() {
       <p>
         Fun fact: This is what people use for those NFC Tags videos. You can check the code to see how it works.
       </p>
+      <p></p>
+      <h2>Genshin Impact</h2>
       <a href="genshin://">Open Genshin - Global</a>
       <p></p>
       <a href="yuanshen://">Open Genshin - CN</a>
       <p></p>
       <a href="yuanshengame://">Open Genshin (Either region)</a>
       <p></p>
+      <h3>Open Genshin - Cloud</h3>
+      <a href="cloudys://">CN - 1</a>
+      <p></p>
+      <a href="yscloud://">CN - 2</a>
+      <p></p>
+      <a href="cloudysglobal://">Global - 1</a>
+      <p></p>
+      <a href="cloudgenshin://">Global - 2</a>
+      <p></p>
+      <h3>Open Genshin - Vietnam</h3>
+      <a href="genshinvn://">Protocall - 1</a>
+      <p></p>
+      <a href="yuanshengamevn://">Protocall - 2</a>
+      <p></p>
+      <h2>Honkai: Star Rail</h2>
       <a href="honkaistarrail://">Open Star Rail - Global</a>
       <p></p>
       <a href="starrailmihoyo://">Open Star Rail - CN</a>
       <p></p>
+      <a href="honkaistarrailvn://">Open Star Rail - Vietnam</a>
+      <p></p>
+      <h3>Open Star Rail - Cloud</h3>
+      <a href="cloudrpg://">CN - 1</a>
+      <p></p>
+      <a href="srcloud://">CN - 2</a>
+      <p></p>
+      <h2>Zenless Zone Zero</h2>
       <a href="zenlessglobal://">Open Zenless - Global</a>
       <p></p>
       <a href="zenless://">Open Zenless - CN</a>
       <p></p>
+      <a href="zenlessvn://">Open Zenless - Vietnam</a>
+      <p></p>
+      <h2>Honkai Impact 3rd</h2>
       <a href="bh3global://">Open HI3rd - Global</a>
       <p></p>
       <a href="bh3oversea://">Open HI3rd - SEA</a>
@@ -36,6 +64,11 @@ function Mobile() {
       <a href="bh3kr://">Open HI3rd - KR</a>
       <p></p>
       <a href="bh3jp://">Open HI3rd - JP</a>
+      <p></p>
+      <h3>Open HI3rd - Vietnam</h3>
+      <a href="bh3overseavn://">Protocall - 1</a>
+      <p></p>
+      <a href="bh3vn://">Protocall - 2</a>
       <p></p>
     </div>
   );
