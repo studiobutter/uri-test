e85e4ddb7778e09c1ec5d82a685a1ef37e22e731
studiobuttermedia <bebosua05@yahoo.com.vn>
2024-09-24 19:06:06 +0700

    Add redirect pages

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,8 @@ import Home from './pages/home';
 import BBS from './pages/miyoushe';
 import HoYoLAB from './pages/hoyolab';
 import Mobile from './pages/mobile'
+import Genshin from './pages/redirects/genshin';
+import Sky from './pages/redirects/sky';
 // Testing
 
 function AppRoutes() {
@@ -14,6 +16,8 @@ function AppRoutes() {
         <Route path="/mobile" element={<Mobile />} />
         <Route path="/hoyolab" element={<HoYoLAB />} />
         <Route path="/bbs" element={<BBS />} />
+        <Route path="/redirects/genshin" element={<Genshin />} />
+        <Route path="/redirects/sky" element={<Sky />} />
       </Routes>
     </Router>
   );
