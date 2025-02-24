5874f55213d5b24f7967c547fbe7e8b34fa5aec0
studiobutter <bebosua05@yahoo.com.vn>
2025-02-24 17:31:15 +0700

    Whoops, forgot https

--- src/pages/mobile.js
+++ src/pages/mobile.js
@@ -15,9 +15,9 @@ function Mobile() {
       <p></p>
       <a href='x-safari-https://example.com'>iOS/iPadOS</a>
       <p></p>
-      <a href="example.com" onclick="window.open(this.href, '_blank')">Android - 1</a>
+      <a href="https://example.com" onclick="window.open(this.href, '_blank')">Android - 1</a>
       <p></p>
-      <a href="example.com" onclick="window.open(this.href, '_system')">Android - 2</a>
+      <a href="https://example.com" onclick="window.open(this.href, '_system')">Android - 2</a>
       <p></p>
       <a href="https://example.com" onclick="window.open('https://example.com', '_system'); return false;">Android - 3</a>
       <p></p>
